8f29b6ca313b8fb8b4a70c940146baaad085f7aa
Joshua Hirst <hirst.joshua@me.com>
2018-11-29 12:09:45 +0000

    Created simple header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './Header.css';
+
+class Header extends PureComponent {
+  render() {
+    return (
+      <div className='header'>
+        <h1>AQUATIC</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
