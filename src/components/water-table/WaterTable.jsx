f14157bb0465fee6681afb4b5920a8333f4bdc55
Joshua Hirst <hirst.joshua@me.com>
2018-12-03 14:24:44 +0000

    Display table data raw

--- src/components/water-table/WaterTable.jsx
+++ src/components/water-table/WaterTable.jsx
@@ -1,11 +1,21 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import './WaterTable.css';
 
 class WaterTable extends PureComponent {
+  renderRows = () => {
+    const data = this.props.data;
+
+    return Object.keys(data).map((key) => {
+      const value = data[key];
+      return <div className='water-table__row'>{`${value.date} ph${value.ph}`}</div>
+    });
+  };
+
   render() {
     return (
       <div className='water-table'>
-
+        {this.renderRows()}
       </div>
     );
   }
